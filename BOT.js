f7ca6757abd649002b6725e8c07fc9ac4f3a9be9
TheLikus <61202701+TheLikus@users.noreply.github.com>
2020-02-20 11:15:19 +0100

    Add files via upload

---
+++ BOT.js
@@ -0,0 +1,69 @@
+const tmi = require("tmi.js")
+
+
+// changed this var from channel to channelName 
+var channelName = "TheLikus"
+// this is the prefix for your commands
+var prefix = "!"
+
+var config = {
+    options: {
+        debug: true
+    }, 
+    connection: {
+        cluster: "aws", 
+        reconnect: true
+    },
+    identity: {
+        username: "TheLikus_BOT",
+        // get yours at http://twitchapps.com/tmi
+        password: "oauth:kgpwxqlqdho4v6dikbp69miqkr1smh"
+    },
+    channels: [channelName]
+}
+
+var client = new tmi.client(config)
+client.connect();
+
+client.on("connected", (address, port) => {
+})
+
+setInterval(() => {
+     client.say(channelName, "Hello")
+    }, 600000);
+
+    // Chat Commands
+client.on("chat", (channel, user, message, self) => {
+    if (self) return;
+    
+    if (message === "hi") {
+        client.say(channelName, "hey pal.")
+    } 
+
+     //Göteborgar mätare.
+    
+     if (message === '!Göteborgare') {
+        const num = rollDice();
+        client.say(channelName, ` ${user.username} Du är ${num}% Göteborgare!`);
+        console.log(`* Executed ${message} command`);
+      } else {
+        console.log(`* Unknown command ${message}`);
+      }
+    
+      function rollDice () {
+        const sides = 100;
+        return Math.floor(Math.random() * sides) + 1;
+      }
+
+    // cmd handler
+    const args = message.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    try {
+        let commandFile = require(`./commands/${cmd}.js`)
+        commandFile.run(client, message, args, user, channel, self) 
+    } catch (err) {
+        return;
+    }
+
+   
+})
